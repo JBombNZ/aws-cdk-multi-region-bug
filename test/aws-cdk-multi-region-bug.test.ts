05189b63e21762c17c13942ff3b3cd4ab55db1f6
Jeremy <jeremy@likefury.com>
2023-04-19 12:57:37 +1200

    initial cdk

---
+++ test/aws-cdk-multi-region-bug.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkMultiRegionBug from '../lib/aws-cdk-multi-region-bug-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-multi-region-bug-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkMultiRegionBug.AwsCdkMultiRegionBugStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
